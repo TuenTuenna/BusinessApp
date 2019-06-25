a1cfddef98d5d38415a68c329b42677c87f87041
kor216465 <kor216465@gmail.com>
2019-06-26 01:23:26 +0900

    Initial Commit

---
+++ TuenTuennaBusinessApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TuenTuennaBusinessApp
+//
+//  Created by Jeff Jeong on 2019. 6. 26..
+//  Copyright © 2019년 Tuentuenna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TuenTuennaBusinessApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TuenTuennaBusinessApp
+//
+//  Created by Jeff Jeong on 2019. 6. 26..
+//  Copyright © 2019년 Tuentuenna. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
